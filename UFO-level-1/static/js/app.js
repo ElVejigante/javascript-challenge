4dd561f1f7706ff8f99532046a2f294eb446c9b8
ElVejigante <ernesto.cancel@gmail.com>
2021-05-04 14:06:11 -0400

    keys values print to console

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,11 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select('tbody');
+
+tableData.forEach(function(ufo){
+    var row = tbody.append('tr');
+    Object.entries(ufo).forEach(function([key, value]){
+        console.log(key, value);
+    });
+});
