0588c7a7268025f5976a831a995b5699feb4825e
ElVejigante <ernesto.cancel@gmail.com>
2021-05-04 14:46:34 -0400

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,12 +1,24 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-var tbody = d3.select('tbody');
-
+// Select table body
+var tBody = d3.select('tbody');
+// loop through data
 tableData.forEach(function(ufo){
-    var row = tbody.append('tr');
+// append data to each table row
+    var row = tBody.append('tr');
+// get key-value pairs
     Object.entries(ufo).forEach(function([key, value]){
-        console.log(key, value);
+        //console.log(key, value);
+// insert text values into cell
+        var cell = row.append('td').text(value);
     });
 });
+
+// Button Function
+var button = d3.select('#filter-btn');
+
+button.on('click', function(){
+//select input element html
+    var inputElement = d3.select('.form-control');
+})
